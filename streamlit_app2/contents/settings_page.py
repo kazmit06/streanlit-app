08f5f1bb62a1fe4f2a59f7c938c9ae028f556a33
kazmit06 <kaz.i18be66nu@gmail.com>
2025-05-11 12:18:16 +0900

    modified docker setting

--- streamlit_app2/contents/settings_page.py
+++ streamlit_app2/contents/settings_page.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("設定ページ")
 st.write("アプリの設定を行います。")
+
+st.write('Plotlyでデータ出力')
+data_canada = px.data.gapminder().query("country=='Canada'")
+fig = px.bar(data_canada,x='year',y='pop')
+st.plotly_chart(fig, use_container_width=True)
